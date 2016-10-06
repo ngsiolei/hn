191ae548cc0e2847549dfd814037a2dd4180f0f5
ngsiolei <ngsiolei@gmail.com>
2016-10-07 04:19:21 +0800

    implement page navigation

--- index.js
+++ index.js
@@ -1,82 +1,129 @@
+var fs = require('fs');
+var util = require('util');
 var firebase = require('firebase');
 var open = require('open');
 var termkit = require('terminal-kit');
 var term = termkit.terminal;
-term.clear();
 var document = term.createDocument();
+var columnMenu = null;
+var itemsCache = {};
+var topStoryIds = [];
+var storiesPerPage = 10;
+var currentPage = 1;
 
-function onSubmit(value) {
-	term.saveCursor();
-	term.moveTo.styleReset.eraseLine(1, 22, '%s\n', value);
-	term.restoreCursor();
-  open(value);
-}
+term.grabInput({mouse: false});
+term.clear();
+
+var updateStoryMeta = function () {
+  if (columnMenu && columnMenu.focusChild) {
+    var v = columnMenu.focusChild.value;
+    var msg = util.format(
+      'by %s, at %s, score: %s, comments: %s',
+      v[1],
+      formatDate(v[2]),
+      v[3],
+      v[4]
+    );
+  } else {
+    msg = '';
+  }
+  msg = ' ' + msg;
+  term.saveCursor();
+  term.moveTo.eraseLine(1, 20, '%s\n', msg);
+  term.restoreCursor();
+};
+
+var updateStatus = function (msg) {
+  if (!msg) {
+    msg = 'j: down | k: up | h: prev page | l: next page | CTRL+C: exit';
+  }
+  msg = ' ' + msg;
+  term.saveCursor();
+  term.moveTo.eraseLine(1, 22, '%s\n', msg);
+  term.restoreCursor();
+};
 
 term.on('key', function (key) {
-	switch (key) {
-		case 'CTRL_C' :
-			term.grabInput( false );
-			term.hideCursor( false );
-			term.styleReset();
-			term.clear();
-			process.exit();
-			break;
-	}
+  switch (key) {
+    case 'CTRL_C':
+      term.grabInput( false );
+      term.hideCursor( false );
+      term.styleReset();
+      term.clear();
+      process.exit();
+      break;
+    case 'h':
+      if (currentPage > 1) {
+        createMenu(currentPage - 1);
+      }
+      break;
+    case 'l':
+      createMenu(currentPage + 1);
+      break;
+    default:
+      updateStoryMeta();
+  }
 });
 
 firebase.initializeApp({
   databaseURL: 'https://hacker-news.firebaseio.com'
 });
-
 var db = firebase.database();
-
-var itemsCache = {};
-var topStoryIds = [];
-var storiesPerPage = 10;
+updateStatus('downloading...');
 
 db.ref('v0/topstories').once('value', function (snapshot) {
   topStoryIds = snapshot.val();
-  //console.log('[INFO] topStoryIds fetched');
-  fetchItemsByPage(1).then(function (values) {
+  log('topStoryIds fetched');
+  createMenu(1);
+}, function (err) {
+  log(err);
+});
+
+var createMenu = function (page) {
+  fetchItemsByPage(page).then(function (values) {
+    currentPage = page;
     var items = [];
-    values.forEach(function (v) {
-      //console.log(v.title);
+    values.forEach(function (v, i) {
+      var index = (currentPage - 1) * storiesPerPage + (i + 1);
       items.push({
-        content: '* ' + v.title,
-        value: v.url
+        content: index + ') ' + v.title,
+        value: [
+          v.url,
+          v.by,
+          v.time,
+          v.score ? v.score : 0,
+          v.kids ? v.kids.length : 0
+        ]
       });
     });
-    var columnMenu = termkit.ColumnMenu.create({
+    if (columnMenu && columnMenu.destroy && typeof columnMenu.destroy === 'function') {
+      columnMenu.destroy();
+    }
+    columnMenu = termkit.ColumnMenu.create({
       buttonFocusAttr: {bgColor: 'white', color: 'black', bold: false},
-      buttonBlurAttr: {bgColor: 'black', color: 'gray', bold: false},
+      buttonBlurAttr: {bgColor: 'black', color: 'white', bold: false},
       keyBindings: {j: 'next', k: 'previous', UP: 'previous', DOWN: 'next'},
       parent: document,
       items: items,
     });
-    columnMenu.on('submit', onSubmit);
+    columnMenu.on('submit', function (value) {
+      open(value[0]);
+    });
     document.giveFocusTo(columnMenu);
+    updateStatus();
+    updateStoryMeta();
   }, function (reason) {
-    console.log(reason);
+    log(reason);
   });
-}, function (err) {
-  console.log(err);
-});
-
-/*
-  fetchItem(topStoryIds[0]).then(function (value) {
-    console.log(value);
-  }, function (reason) {
-    console.log(reason);
-  });
-*/
+};
 
 var fetchItem = function (id) {
   return new Promise(function (resolve, reject) {
     if (itemsCache[id]) {
-      //console.log('[INFO] hitting item ' + id + ' in cache');
+      log('hitting item ' + id + ' in cache');
       resolve(itemsCache[id]);
     } else {
-      //console.log('[INFO] fetching item ' + id);
+      log('fetching item ' + id);
       db.ref('v0/item/' + id).once('value', function (snapshot) {
         itemsCache[id] = snapshot.val();
         resolve(itemsCache[id]);
@@ -102,4 +149,32 @@ var fetchItemsByPage = function (page) {
   var end = page * storiesPerPage;
   var ids = topStoryIds.slice(start, end);
   return fetchItems(ids);
+};
+
+var log = function (msg) {
+  var now = new Date();
+  var year = new String(now.getFullYear());
+  var month = new String(now.getMonth() + 1);
+  if (month.length === 1) month = '0' + month;
+  var date = new String(now.getDate());
+  if (date.length === 1) date = '0' + date;
+  var filename = 'hn-cli-' + year + month + date;
+  fs.appendFile('/tmp/' + filename,
+                msg + '\n',
+                function () {});
+};
+
+var formatDate = function (ts) {
+  ts = ts * 1000;
+  var dateObj = new Date(ts);
+  var year = new String(dateObj.getFullYear());
+  var month = new String(dateObj.getMonth() + 1);
+  if (month.length === 1) month = '0' + month;
+  var date = new String(dateObj.getDate());
+  if (date.length === 1) date = '0' + date;
+  var hour = new String(dateObj.getHours());
+  if (hour.length === 1) hour = '0' + hour;
+  var minute = new String(dateObj.getMinutes());
+  if (minute.length === 1) minute = '0' + minute;
+  return year + '-' + month + '-' + date + ' ' + hour + ':' + minute;
 };
